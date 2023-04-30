1a2ce5605b1bd53c90139b900a842d94e4441e95
Niv <bniv000@gmail.com>
2023-04-30 22:12:49 +0300

    fixed api requests

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,5 +1,5 @@
 const BASE_URL = process.env.NODE_ENV === 'production'
-    ? process.env.NEXT_PUBLIC_URL
+    ? `${process.env.NEXT_PUBLIC_URL}/api/`
     : 'http://localhost:3000/api/'
 
 export const httpService = {

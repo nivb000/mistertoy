df44e817d85eb2b217b9bcb4140693c94a503e03
Niv <bniv000@gmail.com>
2023-04-30 19:02:05 +0300

    fixed api route

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,5 +1,5 @@
 const BASE_URL = process.env.NODE_ENV === 'production'
-    ? `${window.location.protocol}/api/`
+    ? 'https://mistertoy-nextjs.vercel.app/api/'
     : 'http://localhost:3000/api/'
 
 export const httpService = {

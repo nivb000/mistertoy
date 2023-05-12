e87454db5f84197f59144764c93a5b8a07e2acf4
Niv <bniv000@gmail.com>
2023-05-12 13:04:03 +0300

    modifying next config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,17 @@ const nextConfig = {
   reactStrictMode: false,
   experimental: {
     appDir: true
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: "/_next/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
